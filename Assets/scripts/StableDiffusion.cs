352321aacf5aa8db48d9c98dbcaa506767975197
Alexandre Tolstenko <tolstenko@gmail.com>
2023-06-01 22:38:48 -0400

    feat: downsoad and install stablediffusion

--- Assets/scripts/StableDiffusion.cs
+++ Assets/scripts/StableDiffusion.cs
@@ -1,18 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Cysharp.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
+using Debug = UnityEngine.Debug;
 
 public class StableDiffusion : MonoBehaviour
 {
+    private Process process = null;
+    
     private static StableDiffusion instance = null;
     public Texture2D stableDiffusionImage = null;
-    
+
     // logging data structure
+    public List<string> logs = new List<string>();
     // general semaphores, isstaring, isdownloading, isready, isrunning, etc
 
     public static StableDiffusion Instance
@@ -22,7 +27,7 @@ public class StableDiffusion : MonoBehaviour
             if (instance == null)
             {
                 instance = new GameObject("StableDiffusionManager").AddComponent<StableDiffusion>();
-                instance.StartCoroutine(instance.StableDiffusionStart());
+                instance.StableDiffusionStart().Forget();
             }
             return instance;
         }
@@ -34,51 +39,124 @@ public class StableDiffusion : MonoBehaviour
             instance = this;
         }
     }
-
-    private IEnumerator StableDiffusionStart()
+    
+    public async UniTaskVoid StableDiffusionStart()
     {
-        // ref. https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Post.html
-        // ref. https://learn.microsoft.com/en-us/dotnet/api/system.io.file.exists?view=net-7.0
-        // ref. unzip with  System.IO.Compression.FileSystem probably
-
-        // download the automatic1111 zip isnt already downloaded
-        if (!File.Exists(Application.persistentDataPath + Path.PathSeparator + "stable-diffusion-webui.zip"))
+        try
         {
-            UnityWebRequest dlreq =
-                new UnityWebRequest(
-                    "https://github.com/AUTOMATIC1111/stable-diffusion-webui/archive/refs/tags/v1.3.0.zip");
-            dlreq.downloadHandler =
-                new DownloadHandlerFile(Application.persistentDataPath + Path.PathSeparator + "stable-diffusion-webui.zip");
-            var op = dlreq.SendWebRequest();
+            // File.Delete(Application.persistentDataPath + Path.DirectorySeparatorChar + "stable-diffusion-webui.zip");
+            // Directory.Delete(Application.persistentDataPath + Path.DirectorySeparatorChar + "stablediffusion", true);
+            // ref. https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Post.html
+            // ref. https://learn.microsoft.com/en-us/dotnet/api/system.io.file.exists?view=net-7.0
+            // ref. unzip with  System.IO.Compression.FileSystem probably
+
+            // download the automatic1111 zip isnt already downloaded
+            if (!File.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + "stable-diffusion-webui.zip"))
+            {
+                UnityWebRequest dlreq =
+                    new UnityWebRequest(
+                        "https://github.com/AUTOMATIC1111/stable-diffusion-webui/archive/refs/tags/v1.3.0.zip");
+                dlreq.downloadHandler =
+                    new DownloadHandlerFile(Application.persistentDataPath + Path.DirectorySeparatorChar +
+                                            "stable-diffusion-webui.zip");
+                var op = dlreq.SendWebRequest();
+
+                logs.Add("Downloaded");
+
+                while (!op.isDone)
+                {
+                    //here you can see download progress
+                    logs.Add("StableDiffusion Download: " + (dlreq.downloadedBytes / 1000).ToString() + "KB");
+                    await UniTask.Yield();
+                }
+
+                if (dlreq.isNetworkError || dlreq.isHttpError)
+                {
+                    logs.Add(dlreq.error);
+                }
+                else
+                {
+                    logs.Add("download success");
+                }
+            }
+
+            // unzip if it is not unzipped already
+            if (!Directory.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + "stablediffusion"))
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(
+                    Application.persistentDataPath + Path.DirectorySeparatorChar + "stable-diffusion-webui.zip",
+                    Application.persistentDataPath + Path.DirectorySeparatorChar + "stablediffusion");
+                logs.Add("unzip success");
+            }
+
+            // todo: update stablediffusion
+            // start the python server
+            bool isWin = Application.platform == RuntimePlatform.WindowsEditor ||
+                               Application.platform == RuntimePlatform.WindowsPlayer;
             
-            while (!op.isDone)
+            // get first subdirectory
+            string subdir =
+                Directory.GetDirectories(Application.persistentDataPath + Path.DirectorySeparatorChar +
+                                         "stablediffusion")[0];
+            string webui = "webui." + (isWin ? "bat" : "sh");
+
+            string fullpath = subdir + Path.DirectorySeparatorChar + webui;
+            logs.Add(fullpath);
+
+            // todo: improve this for windows and other platforms
+            // make the webui executable
+            if (!isWin)
             {
-                //here you can see download progress
-                Debug.Log( "StableDiffusion Download: " + (dlreq.downloadedBytes / 1000).ToString() + "KB");
-                yield return null;
+                process = new Process()
+                {
+                    StartInfo =
+                    {
+                        FileName = "chmod",
+                        Arguments = "+x " + fullpath
+                    },
+                };
+                process.Start();
+                process.WaitForExit();
+                logs.Add("chmod success");
             }
             
-            if (dlreq.isNetworkError || dlreq.isHttpError)
+            await UniTask.Yield();
+
+            // run webui
+            process = new Process
             {
-                Debug.Log(dlreq.error);
-            }
-            else
+                StartInfo = new ProcessStartInfo
+                {
+                    // WorkingDirectory = subdir,
+                    // FileName = webui,
+                    FileName = fullpath,
+                    Arguments = "--api --disable-console-progressbars --nowebui",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                },
+            };
+
+            // wait for the server to be ready
+            // start capturing the logs and outputting them to the console or other logging system
+            logs.Add("before start");
+            process.Start();
+            logs.Add("started");
+            while (!process.StandardOutput.EndOfStream)
             {
-                Debug.Log("download success");
+                string line = process.StandardOutput.ReadLine();
+                logs.Add(line);
             }
+
+            // change the ready state
+            await UniTask.Yield();
         }
-        
-        // unzip if it is not unzipped already
-        if (!Directory.Exists(Application.persistentDataPath + Path.PathSeparator + "stablediffusion")) {
-            System.IO.Compression.ZipFile.ExtractToDirectory(Application.persistentDataPath + Path.PathSeparator + "stable-diffusion-webui.zip", Application.persistentDataPath + Path.PathSeparator + "stablediffusion");
-            Debug.Log("unzip success");
+        catch (Exception e)
+        {
+            logs.Add(e.Message);
+            logs.Add(e.StackTrace);
+            throw;
         }
-        
-        // start the python server
-        // wait for the server to be ready
-        // start capturing the logs and outputting them to the console or other logging system
-        // change the ready state
-        yield return null;
     }
     
     // shutdown should be blocking
@@ -118,5 +196,14 @@ public class StableDiffusion : MonoBehaviour
     {
         StableDiffusionStop();
         instance = null;
+    }
+
+    void Update()
+    {
+        var logsClone = logs; // to prevent concurrent modification
+        foreach(string log in logsClone) {
+            Debug.Log(log);
+            logs.Remove(log); // needs improvement on concurrency from mainthead consuming and the producer thread logging
+        }
     }
 }
